50790c05e422212d0a39b0fb0f1081585927b1b2
Dilsher Balouch <dilsherb@gmail.com>
2023-01-09 16:03:52 +0300

    HTML and some JS added

---
+++ main.js
@@ -0,0 +1,12 @@
+const books = [
+    {
+        id: 1,
+        title: 'Book 1',
+        author: 'Author 1'
+    },
+    {
+        id: 2,
+        title: 'Book 2',
+        author: 'Author 2'
+    },
+]
